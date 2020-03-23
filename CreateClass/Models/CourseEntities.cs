a0ff1b86e08868f005e61daaeed201b65937c292
Jesuraj77 <isysraju2001@gmail.com>
2020-03-23 16:28:29 +0530

    code  added

---
+++ CreateClass/Models/CourseEntities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    public class CourseEntities
+
+    {
+        //need to implement property
+    }
+}
---
+++ CreateClass/Services/Course.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreateClass
+{
+    public class Course
+    {
+        public CourseEntities GetInfo()
+        {
+            
+            CourseEntities courseentities = new CourseEntities();
+            return courseentities;
+        }
+    }
+}
